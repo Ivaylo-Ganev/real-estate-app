336d5be14a513198e3cd44ebf93ec191b448a133
Ivaylo Ganev <ivayloganev@gmail.com>
2023-04-04 00:16:38 +0300

    Set form validation

--- src/components/CreateProperty/CreateProperty.js
+++ src/components/CreateProperty/CreateProperty.js
@@ -1,10 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { useForm } from "../../hooks/useForm";
 import { PropertyContext } from "../../contexts/PropertyContext";
 
 export const CreateProperty = () => {
     const {onCreateSubmitHandler} = useContext(PropertyContext);
+    const [formErrors, setFormErrors] = useState({
+        propertyName: '',
+        location: ''
+    });
 
     const {values, onChangeHandler, onSubmit} = useForm({
         propertyName: '',
@@ -17,19 +21,60 @@ export const CreateProperty = () => {
         description: ''
     }, onCreateSubmitHandler);
 
+    const onBlur = (e) => {
+        e.preventDefault();
+        const currentValue = e.target.value;
+        const currentName = e.target.name;
+        
+        if (currentName === 'propertyName' && currentValue.length > 3) {
+            const propertyNameError = "Property name too long"
+            setFormErrors(state => ({...state, [currentName]: propertyNameError}));
+        } else if (currentName === 'propertyName' && currentValue.length <= 3) {
+            setFormErrors(state => ({...state, [currentName]: ''}));
+        }
+
+        if (currentName === 'location' && currentValue.length > 3) {
+            const locationNameError = "Location name too long"
+            setFormErrors(state => ({...state, [currentName]: locationNameError}));           
+        } else if (currentName === 'location' && currentValue.length <= 3) {
+            setFormErrors(state => ({...state, [currentName]: ''}));
+        }
+    }
 
     return (
         <section id="create-page" className="auth">
+            
         <form id="create" action="POST" onSubmit={onSubmit}>
             <div className="container">
 
                 <h1>Create Property</h1>
                 <label htmlFor="propertyName">Property name:</label>
-                <input type="text" id="propertyName" name="propertyName" placeholder="Enter property name..." value={values.propertyName} onChange={onChangeHandler}/>
-
+                <input
+                type="text"
+                id="propertyName"
+                name="propertyName"
+                placeholder="Enter property name..."
+                value={values.propertyName}
+                onChange={onChangeHandler}
+                onBlur={onBlur}
+                />
+                {formErrors['propertyName'] && (
+                <p className="form-error">{formErrors.propertyName}</p>
+                )}
+                
                 <label htmlFor="location">Location:</label>
-                <input type="text" id="location" name="location" placeholder="Enter location..." value={values.location} onChange={onChangeHandler}/>
-
+                <input
+                type="text"
+                id="location"
+                name="location"
+                placeholder="Enter location..."
+                value={values.location}
+                onChange={onChangeHandler}
+                onBlur={onBlur}
+                />
+                {formErrors['location'] && (
+                <p className="form-error">{formErrors.location}</p>
+                )}
                 <label htmlFor="price">Price in EUR:</label>
                 <input type="number" id="price" name="price" min="1" value={values.price} onChange={onChangeHandler}/>
 
